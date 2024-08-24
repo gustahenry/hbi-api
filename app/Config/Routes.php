1deb2256b41684bd005c158b953f953930a582f5
gustavo henrique <ghgoncalves430@gmail.com>
2024-08-24 16:23:46 -0300

    criado rotas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('contacts', 'Contacts::index');
+$routes->post('contacts', 'Contacts::create');
+$routes->put('contacts/(:num)', 'Contacts::update/$1');
+$routes->delete('contacts/(:num)', 'Contacts::delete/$1');
