c50c1120d2c498928aec6150b1d41c3dc24bfc6e
gustavo henrique <ghgoncalves430@gmail.com>
2024-08-25 16:01:55 -0300

    adicionado user a conexão

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,7 +27,7 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
         'database'     => 'hbi-api',
         'DBDriver'     => 'MySQLi',
@@ -48,6 +48,34 @@ class Database extends Config
             'datetime' => 'Y-m-d H:i:s',
             'time'     => 'H:i:s',
         ],
+    
+    ];
+
+    public array $test = [
+        'DSN'          => '',
+        'hostname'     => 'localhost',
+        'username'     => 'root',
+        'password'     => '',
+        'database'     => 'hbi-api_test',
+        'DBDriver'     => 'MySQLi',
+        'DBPrefix'     => 'tests_',
+        'pConnect'     => false,
+        'DBDebug'      => true,
+        'charset'      => 'utf8mb4',
+        'DBCollat'     => 'utf8mb4_general_ci',
+        'swapPre'      => '',
+        'encrypt'      => false,
+        'compress'     => false,
+        'strictOn'     => false,
+        'failover'     => [],
+        'port'         => 3306,
+        'numberNative' => false,
+        'dateFormat'   => [
+            'date'     => 'Y-m-d',
+            'datetime' => 'Y-m-d H:i:s',
+            'time'     => 'H:i:s',
+        ],
+    
     ];
 
     //    /**
